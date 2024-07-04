ac84c89be08cbe6d7d2eef48ad7b9ee42a51bbb8
Raúl Llanos <llanosraul.v@gmail.com>
2024-07-04 19:12:13 -0400

    test producto con MagicMock

--- tests/producto/tests_producto.py
+++ tests/producto/tests_producto.py
@@ -1,19 +1,57 @@
+# import pytest
+# from producto.models import Producto, Categoria
+
+# @pytest.mark.django_db
+# def tests_producto_creations():
+#     categoria = Categoria.objects.create(
+
+#         nombre = 'test_unitario_categoria'
+#     )
+
+#     producto = Producto.objects.create(
+#         nombre = 'test_producto_nombre',
+#         precio = 23000,
+#         descripcion = 'test_producto_descripcion',
+#         categoria = categoria,
+#         stock = 23,
+#     )
+
+#     assert producto.nombre == 'test_producto_nombre'
+
 import pytest
+from unittest.mock import patch, MagicMock
 from producto.models import Producto, Categoria
 
 @pytest.mark.django_db
-def tests_producto_creations():
-    categoria = Categoria.objects.create(
+@patch('producto.models.Producto')
+@patch('producto.models.Categoria')
+def test_producto_creation(mock_categoria, mock_producto):
+    
+    categoria = MagicMock()
+    categoria.nombre = 'test_unitario_categoria'
+    mock_categoria.objects.create.return_value = categoria
 
-        nombre = 'test_unitario_categoria'
+    
+    producto = MagicMock()
+    producto.nombre = 'test_producto_nombre'
+    producto.precio = 23000
+    producto.descripcion = 'test_producto_descripcion'
+    producto.categoria = categoria
+    producto.stock = 23
+    mock_producto.objects.create.return_value = producto
+
+    
+    categoria_creada = Categoria.objects.create(
+        nombre='test_unitario_categoria'
     )
 
-    producto = Producto.objects.create(
-        nombre = 'test_producto_nombre',
-        precio = 23000,
-        descripcion = 'test_producto_descripcion',
-        categoria = categoria,
-        stock = 23,
+    producto_creado = Producto.objects.create(
+        nombre='test_producto_nombre',
+        precio=23000,
+        descripcion='test_producto_descripcion',
+        categoria=categoria_creada,
+        stock=23,
     )
 
-    assert producto.nombre == 'test_producto_nombre'
+    
+    assert producto_creado.nombre == 'test_producto_nombre'
